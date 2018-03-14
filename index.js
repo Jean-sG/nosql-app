1d98ed75f980430ed093256fef5188580414b7c1
jeang <jeansh.guillaume@gmail.com>
2018-03-14 10:23:30 +0100

    frontend

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('index');
 })
  
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+})
