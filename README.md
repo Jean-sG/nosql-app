dcfcf9d458201220d9917214d82c8ccca78ac5ca
Unknown <paul.lerner@devinci.fr>
2018-03-16 11:08:12 +0100

    docs : update readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nosql-app
+
+## import the data
+You should have curl installed, run
+```
+curl -XPUT "localhost:9200/_bulk" -H "Content-Type: application/json" --data-binary @movies_elastic.json
+```
+
+## run the application
+
+run
+```
+npm install
+```
+in order to install the dependencies, then run
+```
+npm start
+```
+to launch the app, open your favorite browser and go to http://localhost:3000/
--- init_bd.js
+++ init_bd.js
@@ -1,15 +1,6 @@
+
 const command =`curl -XPUT "localhost:9200/_bulk" -H "Content-Type: application/json" --data-binary @movies_elastic.json`;
-const { spawn } = require('child_process');
-const ls = spawn(command);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+const  exec  = require('child_process').exec;
+exec(command);
